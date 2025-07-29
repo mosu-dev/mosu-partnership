fd497868401a7731af2ef349c36f18c736d6c845
kimgho <kgh67811@knu.ac.kr>
2025-07-30 01:37:44 +0900

    feat: 에러 바운더리 추가
    Co-authored-by: KimDaeGeon <toothlessdev@users.noreply.github.com>

--- partnership-app/src/apps/Router.tsx
+++ partnership-app/src/apps/Router.tsx
@@ -3,11 +3,19 @@ import FormPage from "@/pages/FormPage";
 import HomePage from "@/pages/HomePage";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
+import ErrorBoundary from "./ui/ErrorBoundary";
 
 const routes = createRoutesFromElements(
     <Fragment>
         <Route path="/" element={<RootLayout />}>
-            <Route index element={<HomePage />} />
+            <Route
+                index
+                element={
+                    <ErrorBoundary fallback={() => <div className="text-center">만료된 신청 폼입니다.</div>}>
+                        <HomePage />
+                    </ErrorBoundary>
+                }
+            />
             <Route path="/form" element={<FormPage />} />
         </Route>
     </Fragment>
