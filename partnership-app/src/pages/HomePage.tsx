8e3e39e915f205ac9faf1c10fbb1b5fa6a022f8f
kimgho <kgh67811@knu.ac.kr>
2025-07-30 01:48:58 +0900

    feat: 로딩 중 스피너 추가 및 검색 파라미터를 포함한 링크 수정
    Co-authored-by: KimDaeGeon <52105661+toothlessdev@users.noreply.github.com>

--- partnership-app/src/pages/HomePage.tsx
+++ partnership-app/src/pages/HomePage.tsx
@@ -1,18 +1,16 @@
+import { Spinner } from "@/apps/ui/Spinner";
 import { Button } from "@/components/ui/button";
 import { useLoadRegisterInfoDocs } from "@/hooks/useLoadRegisterInfoDocs";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 
 export default function HomePage() {
-    const { registerInfoDocs } = useLoadRegisterInfoDocs({
-        examYear: "2025",
-        examMonth: "10",
-        examDate: "19",
-        examDay: "일",
-        refundDueMonth: "10",
-        refundDueDate: "12",
-        refundDueTime: "23:59",
-    });
+    const { registerInfoDocs, isPending } = useLoadRegisterInfoDocs();
+    const [searchParams] = useSearchParams();
+
+    if (isPending) return <Spinner />;
+
+    if (!registerInfoDocs) throw new Error("만료된 폼");
 
     return (
         <Fragment>
@@ -23,9 +21,7 @@ export default function HomePage() {
                 }}
             />
             <Button asChild variant="default" className="w-full h-[48px]">
-                <Link to="/form" replace>
-                    다음으로
-                </Link>
+                <Link to={`/form/?${searchParams.toString()}`}>다음으로</Link>
             </Button>
         </Fragment>
     );
