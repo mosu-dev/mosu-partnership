edd2d861efe5a34518bf495f22b97f73891a4441
toothlessdev <toothless042@gmail.com>
2025-08-12 05:18:12 +0900

    fix: ToastContainer 추가 및 설정

--- partnership-app/src/App.tsx
+++ partnership-app/src/App.tsx
@@ -2,11 +2,24 @@ import { router } from "@/apps/Router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { queryClient } from "./apps/lib/query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function MockTestForm() {
     return (
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
         </QueryClientProvider>
     );
 }
