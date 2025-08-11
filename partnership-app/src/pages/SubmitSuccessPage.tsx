26a05851dcdbd21dd99e36a8468948154ba18ca5
kimgho <kgh67811@knu.ac.kr>
2025-08-11 17:51:43 +0900

    fix: 계좌번호 길이를 생각하여 박스 길이 조정

--- partnership-app/src/pages/SubmitSuccessPage.tsx
+++ partnership-app/src/pages/SubmitSuccessPage.tsx
@@ -17,7 +17,7 @@ export default function SubmitSuccessPage() {
                 <p className="text-md text-center text-[#909090] mb-0">
                     2일 내에 가상계좌로 입금완료시 신청이 완료됩니다.
                 </p>
-                <div className="p-4 bg-gray-50 rounded-md">
+                <div className="p-4 bg-gray-50 rounded-md w-[350px]">
                     <p className="flex justify-between">
                         <span>- 계좌번호:</span>
                         <span className="font-semibold">
